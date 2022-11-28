d0f8a48f516440eaf63d47024531ad7143e9706d
Cristóbal Sifón <cristobal.sifon@pucv.cl>
2022-11-28 00:13:27 +0000

    fix mpl deprecation #26

--- src/plottery/plotutils.py
+++ src/plottery/plotutils.py
@@ -164,11 +164,11 @@ def update_rcParams(dict={}):
     default['font.size'] = 22
     default['legend.fontsize'] = 18
     default['lines.linewidth'] = 2
-    default['text.latex.preamble']=['\\usepackage{amsmath}']
+    default['text.latex.preamble'] = '\\usepackage{amsmath}'
     # Matthew Hasselfield's color-blind-friendly style
-    default['axes.prop_cycle'] = \
-        cycler(color=['#2424f0','#df6f0e','#3cc03c','#d62728','#b467bd',
-                      '#ac866b','#e397d9','#9f9f9f','#ecdd72','#77becf'])
+    default['axes.prop_cycle'] \
+        = cycler(color=['#2424f0','#df6f0e','#3cc03c','#d62728','#b467bd',
+                        '#ac866b','#e397d9','#9f9f9f','#ecdd72','#77becf'])
     for key in default:
         # some parameters are not valid in different matplotlib functions
         try:
